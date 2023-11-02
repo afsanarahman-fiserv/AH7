5b0f538eba565aeacb7635fab356887fe15212f8
Rahman <afsana.rahman@fiserv.com>
2023-11-02 13:10:53 -0400

    AH7 complete - inital submission

--- Source.java
+++ Source.java
@@ -10,8 +10,12 @@ class Node<T> {
 
 class Source {
   public static int sumList(Node<Integer> head) {
-    // todo
-    return;
+    int sum = 0;
+    while(head != null) {
+      sum += head.val;
+      head = head.next;
+    }
+    return sum;
   }
 
   public static void main(String[] args) {
@@ -20,15 +24,34 @@ class Source {
     Node<Integer> c = new Node<>(3);
     Node<Integer> d = new Node<>(-1);
     Node<Integer> e = new Node<>(7);
-
     a.next = b;
     b.next = c;
     c.next = d;
     d.next = e;
-
-    // 2 -> 8 -> 3 -> -1 -> 7
-    
     System.out.println("Sum of my list: ");
     System.out.println(Source.sumList(a));
+
+    // extra credit
+    System.out.println("Testing a second list ...");
+    // list: 0 -> 1 -> 2 -> 3 -> 4 -> 5
+    // populate list
+    Node<Integer> head = new Node<>(0);
+    Node<Integer> ptr = head;
+    for(int i = 1; i <= 5; i++) {
+      ptr.next = new Node<>(i);
+      ptr = ptr.next;
+    }
+
+    // print list to verify
+    for(ptr = head; ptr != null; ptr = ptr.next) {
+      if(ptr.next == null) {
+        System.out.println(ptr.val);
+      } else {
+        System.out.print(ptr.val + " -> ");
+      }
+    }
+
+    // get sum: expected = 15
+    System.out.println("Sum of second list: " + Source.sumList(head));
   }
 }
